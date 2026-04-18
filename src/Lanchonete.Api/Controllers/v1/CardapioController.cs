042bbaa165b9a51f508924cdbb99e1a3259cf43d
Arthur Amaral <arthuramaral1509@gmail.com>
2026-04-18 15:47:45 -0300

    add: autenticação cardapio controller

--- src/Lanchonete.Api/Controllers/v1/CardapioController.cs
+++ src/Lanchonete.Api/Controllers/v1/CardapioController.cs
@@ -1,10 +1,12 @@
 using Lanchonete.Application.Dtos.Cardapio;
 using Lanchonete.Application.Dtos.Compartilhado;
 using Lanchonete.Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Lanchonete.Api.Controllers.v1;
 
+[Authorize]
 [ApiController]
 [Route("api/v1/[controller]")]
 public sealed class CardapioController(ICardapioAppService cardapioAppService) : ControllerBase
