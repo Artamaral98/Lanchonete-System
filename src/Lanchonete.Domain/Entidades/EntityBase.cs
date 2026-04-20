41b60a06566681ac93f8af08a8af021237f0e709
unknown <arthuramaral1509@gmail.com>
2026-04-20 14:36:31 -0300

    fix: data criacao pedido

--- src/Lanchonete.Domain/Entidades/EntityBase.cs
+++ src/Lanchonete.Domain/Entidades/EntityBase.cs
@@ -3,5 +3,5 @@ namespace Lanchonete.Domain.Entidades;
 public abstract class EntityBase
 {
     public Guid Id { get; set; } = Guid.NewGuid();
-    public DateTime DataCriacao { get; set; } = DateTime.UtcNow;
+    public DateTime DataCriacao { get; set; } = DateTime.Now;
 }
